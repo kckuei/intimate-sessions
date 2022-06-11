18e54a5ad47ad257df9dc36dabb2cfb6af0c9b1b
meh coder <crappyc0d3r@gmail.com>
2022-06-11 14:09:36 -0700

    first session

---
+++ first-sesion.py
@@ -0,0 +1,63 @@
+import random 
+import re
+from sexytimes import intimateSessions
+
+S = intimateSessions()
+
+S.positions = ['missionary', 'doggy', 'standing' '69','reverse cowgirl']
+
+S.set_position(positions[0])
+
+def special_request(positions):
+
+  bangs = 0
+  sexy_time = true
+
+  while sexy_time :
+  
+       if (bangs > 20) and (bangs % (10 * random.randint(1,5)) == 0):
+       set_position(positions[random.randint(1,5)])
+
+      response = raw_input()
+      
+      If response.is_moan():
+         finger(20, tease=true)
+         print("you like that don't you 😏")
+         finger(10, tease=true)
+         pull_out(slow)
+         do_oral(key=😛)
+         insertion()
+         play_in_the_shallow(5)
+         go_deeper()
+         full_depth()
+         
+      elif re.search(response, '%talk dirty to me%'):
+         print("you naughty girl. you're my little cumbucket aren't you? time to punish you. 🍆")
+         pinch_nipples(which="left")
+         squeeze_boobs("firm", which="both")
+         arrest_limbs(where="behind head')
+         go_deeper()
+         
+      elif any([w in ('faster', 'right there', 'just like that', 'fuck me') for w in response.split(' ')]):
+         intensify(exp(bangs))
+         print("tell me what you want")
+         
+      elif response.almostorgasm():
+         synchronize_rhthyms()
+         choke("lightly")
+         kiss("passionately", lip_bight="gently")
+      
+      elif response.isorgasm()
+        embrace()
+        sexy_time = false
+
+      do_bang()
+      bangs += 1
+      
+  cuddle_time = (true, ['netflix','chill'])
+  return cuddle_time
+
+S.custom(special_request, *S.positions )
+
+S.run()
+
